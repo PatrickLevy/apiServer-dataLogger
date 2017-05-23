c098e94e1dbac1eedcd5f214d2d71389fba9e62e
Patrick Levy <mrpatricklevy@gmail.com>
2017-05-23 08:20:19 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,35 +1,9 @@
-// Version 1
-
-// var http = require('http');
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello New York\n');
-// }).listen(3001);
-// console.log('Server running at http://localhost:3001/');
-
-// Version 2
-// var express = require('express');
-// var app = express();
-// app.get('/', function(req, res) {
-//   res.send('Hello Seattle\n');
-// });
-// app.listen(3001);
-// console.log('Listening on port 3001...');
-
-// Version 3
-// var express = require('express');
-// var app = express();
-// require('./routes')(app);
-// app.listen(3001);
-// console.log("Jammin\' on port 3001...");
-
 var express = require('express'),
     mongoose = require('mongoose'),
     fs = require('fs');
 var bodyParser = require('body-parser');
 
-//var mongoUri = 'mongodb://localhost/noderest';
-var mongoUri = 'mongodb://patricklevy:BScsci2015!@ds151018.mlab.com:51018/temperature';
+var mongoUri = 'mongodb://testUser:t3stpw@ds151018.mlab.com:51018/temperature';
 mongoose.connect(mongoUri);
 var db = mongoose.connection;
 db.on('error', function () {
